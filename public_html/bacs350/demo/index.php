8bcc77c841da02a1ad89d97e6e83675f0df01329
Mark Seaman <Mark.Seaman@imac.net>
2019-08-29 15:50:00 -0600

    Build demo code

---
+++ public_html/bacs350/demo/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Page Title</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <h1>Main Topic</h1>
+    <p>This simple page shows how to hook up an external stylesheet.</p>
+</body>
+
+</html>
